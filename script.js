4641ff12ace8ef9e6fd430975e070b53df8d5b0a
Marvin Karhan <1821400@stud.hs-mannheim.de>
2020-07-21 08:08:20 +0200

    kippelemente map

---
+++ script.js
@@ -0,0 +1,42 @@
+let tippingPoints = [
+    {
+        id: "tippingPoint-1",
+        desc: "Abschmelzen des sommerlichen arktischen Meereises"
+    },
+    {
+        id: "tippingPoint-2",
+        desc: "Abschmelzen des Grönländischen Eisschildes"
+    },
+    {
+        id: "tippingPoint-3",
+        desc: "Abschmelzen des Westantarktischen Eisschildes"
+    },
+    {
+        id: "tippingPoint-4",
+        desc: "Erlahmen der atlantischen thermohalinen Zirkulation"
+    },
+    {
+        id: "tippingPoint-5",
+        desc: "Veränderung der El Niño-Southern Oscillation (ENSO)"
+    },
+    {
+        id: "tippingPoint-6",
+        desc: "Zusammenbruch des indischen Sommermonsuns"
+    },
+    {
+        id: "tippingPoint-7",
+        desc: "Veränderungen im Westafrikanischen Monsunsystem mit Auswirkungen auf Sahara und Sahelzone (mit möglicherweise Ergrünen der Sahara als positivem Kippelement)"
+    },
+    {
+        id: "tippingPoint-8",
+        desc: "Entwaldung des tropischen Regenwaldes"
+    },
+    {
+        id: "tippingPoint-9",
+        desc: "Rückgang borealer Wälder"
+    },
+];
+
+$(document).ready(function(){
+    window.scrollTo(($(document).width()-$(window).width())/2,0);
+});
