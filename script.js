0b8a7a215c9b68dfbca69af3ed89a2de32f93a0d
SimonSchneider <1725441@stud.hs-mannheim.de>
2020-07-21 11:21:28 +0200

    Questions integrated

--- script.js
+++ script.js
@@ -1,39 +1,516 @@
 let tippingPoints = [
     {
         id: "tippingPoint-1",
-        desc: "Abschmelzen des sommerlichen arktischen Meereises"
+        desc: "Abschmelzen des sommerlichen arktischen Meereises",
+        questions: [
+            {
+                question: "Um wie viel Prozent nimmt die Ausdehnung des Eises pro Jahrzent ab?",
+                answers: [
+                    {
+                        value: "8%",
+                        correct: true
+                    },
+                    {
+                        value: "15%",
+                        correct: false
+                    },
+                    {
+                        value: "11%",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Wie stark hat sich die Ausdehnung des Eises seit den 1950er/60er Jahren verringert?",
+                answers: [
+                    {
+                        value: "Mehr als die Hälfte",
+                        correct: true
+                    },
+                    {
+                        value: "Fast garnicht",
+                        correct: false
+                    },
+                    {
+                        value: "Ca drei viertel",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was bewirkt die geringere Eisausdehnung in Bezug auf den Albedo?",
+                answers: [
+                    {
+                        value: "Verstärkt Absorption der solaren Einstrahlung durch das Meerwasser",
+                        correct: true
+                    },
+                    {
+                        value: "Wirkt sich nicht aus",
+                        correct: false
+                    },
+                    {
+                        value: "Verstärkt Reflexion der Sonnenstrahlung",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-2",
-        desc: "Abschmelzen des Grönländischen Eisschildes"
+        desc: "Abschmelzen des Grönländischen Eisschildes",
+        questions: [
+            {
+                question: "Was ist ein großer Unterschied des Grönländischen Eisschildes im Vergleich zur Antarktis?",
+                answers: [
+                    {
+                        value: "An den meisten Stellen erreicht der Eisschild das Meer nicht",
+                        correct: true
+                    },
+                    {
+                        value: "Das Eis hat eine bis zu doppelt so hohe Dichte",
+                        correct: false
+                    },
+                    {
+                        value: "Im Sommert taut der Eisschild komplett ab",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was entdeckten Forscher kürzlich unter der Eisdecke Grönlands?",
+                answers: [
+                    {
+                        value: "Eine Schmelzwasserfläche in der Größe von Irland",
+                        correct: true
+                    },
+                    {
+                        value: "Eine bisher unbekannte Robbenart",
+                        correct: false
+                    },
+                    {
+                        value: "Ölvorkommen in der Größe Bayerns",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was ist die Haupt-Ursache für das Schmelzen der Eisschilde in Grönland?",
+                answers: [
+                    {
+                        value: "Ansteigende Temperaturen im Sommer",
+                        correct: true
+                    },
+                    {
+                        value: "Eindringen von ozeanischem Tiefenwasser",
+                        correct: false
+                    },
+                    {
+                        value: "Eindringen von ozeanischem Tiefenwasser",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-3",
-        desc: "Abschmelzen des Westantarktischen Eisschildes"
+        desc: "Abschmelzen des Westantarktischen Eisschildes",
+        questions: [
+            {
+                question: "Welche Fläche umfasst der auf Land und Ozeanboden ruhende Eisschild?",
+                answers: [
+                    {
+                        value: "2,2 Mio km²",
+                        correct: true
+                    },
+                    {
+                        value: "7,2 Mio km²",
+                        correct: false
+                    },
+                    {
+                        value: "3,6 Mio km²",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Wie groß ist die gegenwärtige Eismasse des Westantarktischen Eisschildes?",
+                answers: [
+                    {
+                        value: "3,6 Mio km³",
+                        correct: true
+                    },
+                    {
+                        value: "1,2 Mio km³",
+                        correct: false
+                    },
+                    {
+                        value: "6,6 Mio km³",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was ist die Haupt-Ursache für die beobachteten Veränderungen der Gletscher und Eisschelfe des Amundsenseesektors?",
+                answers: [
+                    {
+                        value: "Eindringen von ozeanischem Tiefenwasser",
+                        correct: true
+                    },
+                    {
+                        value: "Ausbleibender Niederschlag",
+                        correct: false
+                    },
+                    {
+                        value: "Ansteigende Temperaturen im Sommer",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-4",
-        desc: "Erlahmen der atlantischen thermohalinen Zirkulation"
+        desc: "Erlahmen der atlantischen thermohalinen Zirkulation",
+        questions: [
+            {
+                question: "Was ist eine thermohaline Zirkulation?",
+                answers: [
+                    {
+                        value: "Kombination von Meeresströmungen, die sich zu einem globalen Kreislauf vereinen",
+                        correct: true
+                    },
+                    {
+                        value: "Kombination von Windströmugen, die mindestens die Stärke 10 haben",
+                        correct: false
+                    },
+                    {
+                        value: "Kombination von luftelektrischen Entladungen und Regen",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Auf welcher Fläche treten Zirkulationsströme auf?",
+                answers: [
+                    {
+                        value: "Oberfläche sowie in der Tiefe",
+                        correct: true
+                    },
+                    {
+                        value: "Oberfläche",
+                        correct: false
+                    },
+                    {
+                        value: "In der Tiefe",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Auf wieviel km Tiefe wird das winterlinke Absinken des salzriechen und kalten Meereswassers im Nordatlantik initiiert?",
+                answers: [
+                    {
+                        value: "1 bis 4 km",
+                        correct: true
+                    },
+                    {
+                        value: "4 bis 8 km",
+                        correct: false
+                    },
+                    {
+                        value: "8 bis 12 km",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-5",
-        desc: "Veränderung der El Niño-Southern Oscillation (ENSO)"
+        desc: "Veränderung der El Niño-Southern Oscillation (ENSO)",
+        questions: [
+            {
+                question: "Wie heißt die Abkürzung für El Niño-Southern Oscillation?",
+                answers: [
+                    {
+                        value: "ENSO",
+                        correct: true
+                    },
+                    {
+                        value: "ENO",
+                        correct: false
+                    },
+                    {
+                        value: "EINSO",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was beschreibt der El Niño-Southern Oscillation?",
+                answers: [
+                    {
+                        value: "Ein komplexes gekoppeltes Zirkulationssystem von Erdatmosphäre und Meeresströmung im äquatorialen Pazifik",
+                        correct: true
+                    },
+                    {
+                        value: "Ein unkomplexes gekoppeltes Zirkulationssystem von Erdatmosphäre und Meeresströmung im äquatorialen Pazifik",
+                        correct: false
+                    },
+                    {
+                        value: "Ein unkomplexes Zirkulationssystem in der Erdatmosphäre im äquatorialen Pazifik",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Wieviele Hauptphasen hat der ENSO?",
+                answers: [
+                    {
+                        value: "3",
+                        correct: true
+                    },
+                    {
+                        value: "4",
+                        correct: false
+                    },
+                    {
+                        value: "5",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-6",
-        desc: "Zusammenbruch des indischen Sommermonsuns"
+        desc: "Zusammenbruch des indischen Sommermonsuns",
+        questions: [
+            {
+                question: "Welche Besonderheit weist der indische Sommermonsun im Vergleich zu anderen Monsunen auf?",
+                answers: [
+                    {
+                        value: "Wirkung bis in die oberen Schichten der Troposphäre",
+                        correct: true
+                    },
+                    {
+                        value: "Beeinflusst die Zirkulation im Arabischen Meer",
+                        correct: false
+                    },
+                    {
+                        value: "Sehr niedrige Luftfeuchtigkeit",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Welche Bedeutung hat der Sommermonsun für die Bevölkerung?",
+                answers: [
+                    {
+                        value: "Regenfälle sind überlebenswichtig für die Landwirtschaft",
+                        correct: true
+                    },
+                    {
+                        value: "Gefahr durch plötzliche Stürme",
+                        correct: false
+                    },
+                    {
+                        value: "Nahrungsengpass durch Trockenheit",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Welche Gefahr birgt der indische Sommermonsun?",
+                answers: [
+                    {
+                        value: "Viele Überschwemmungen",
+                        correct: true
+                    },
+                    {
+                        value: "Schwankendes Klima von Jahr zu Jahr",
+                        correct: false
+                    },
+                    {
+                        value: "Plötzliche Schneefälle",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-7",
-        desc: "Veränderungen im Westafrikanischen Monsunsystem mit Auswirkungen auf Sahara und Sahelzone (mit möglicherweise Ergrünen der Sahara als positivem Kippelement)"
+        desc: "Veränderungen im Westafrikanischen Monsunsystem mit Auswirkungen auf Sahara und Sahelzone (mit möglicherweise Ergrünen der Sahara als positivem Kippelement)",
+        questions: [
+            {
+                question: "Was ist das Westafrikanische Monsunsystem?",
+                answers: [
+                    {
+                        value: "Ein gekoppeltes Atmosphäre-Ozean-Land-System",
+                        correct: true
+                    },
+                    {
+                        value: "Ein Athmosphäresystem",
+                        correct: false
+                    },
+                    {
+                        value: "Eine Windkalkulation",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was ist eine Besonderheit des westafrikanischen Monsunsystems?",
+                answers: [
+                    {
+                        value: "Große Unterschiede der Niederschlagsmenge an den verschiedenen Grenzen",
+                        correct: true
+                    },
+                    {
+                        value: "Es gibt keine Trockenzeit",
+                        correct: false
+                    },
+                    {
+                        value: "Der Monsun hält fast das ganze Jahr an",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Was spielt eine entscheidende Rolle im Westafrikanischen Monsunsystem",
+                answers: [
+                    {
+                        value: "Wassertemperatur im guineischen Golf",
+                        correct: true
+                    },
+                    {
+                        value: "Erhöhte Temperaturen der letzten Jahre",
+                        correct: false
+                    },
+                    {
+                        value: "Steigender CO2 Austoß ",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-8",
-        desc: "Entwaldung des tropischen Regenwaldes"
+        desc: "Entwaldung des tropischen Regenwaldes",
+        questions: [
+            {
+                question: "Wieviele Male mehr speichern Regenwälder CO2 in den Pflanzen als heimische Wälder?",
+                answers: [
+                    {
+                        value: "6x",
+                        correct: true
+                    },
+                    {
+                        value: "4x",
+                        correct: false
+                    },
+                    {
+                        value: "2x",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Wieviel Prozent macht die (Brand-)Rodung der tropischen Regenwälder des weltweitenb Ausstoßes von CO2 aus?",
+                answers: [
+                    {
+                        value: "Zwischen 8 und 11 %",
+                        correct: true
+                    },
+                    {
+                        value: "Zwischen 13 und 16%",
+                        correct: false
+                    },
+                    {
+                        value: "Zwischen 21 und 23 %",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Wieviel Hektar Wald wurde im Jahr 2017 abgeholzt?",
+                answers: [
+                    {
+                        value: "29,4 Millionen",
+                        correct: true
+                    },
+                    {
+                        value: "22,7 Millionen",
+                        correct: false
+                    },
+                    {
+                        value: "27,3 Millionen",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
     {
         id: "tippingPoint-9",
-        desc: "Rückgang borealer Wälder"
+        desc: "Rückgang borealer Wälder",
+        questions: [
+            {
+                question: "In welcher Klimazone befinden sich boreale Wälder?",
+                answers: [
+                    {
+                        value: "Kaltgemäßigte Zone",
+                        correct: true
+                    },
+                    {
+                        value: "Subtropen",
+                        correct: false
+                    },
+                    {
+                        value: "Polarzone",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Welche Bäume finden sich hauptsätzlich im borealen Nadelwald wieder?",
+                answers: [
+                    {
+                        value: "Kiefern, Fichten und Tannen",
+                        correct: true
+                    },
+                    {
+                        value: "keine",
+                        correct: false
+                    },
+                    {
+                        value: "Eichen, Birken und Tannen",
+                        correct: false
+                    }
+                ]
+            },
+            {
+                question: "Welches ist ein anderes Wort für die Klimazone borealer Wälder?",
+                answers: [
+                    {
+                        value: "Taiga",
+                        correct: true
+                    },
+                    {
+                        value: "Tundra",
+                        correct: false
+                    },
+                    {
+                        value: "Terraega",
+                        correct: false
+                    }
+                ]
+            }
+        ]
     },
 ];
 
